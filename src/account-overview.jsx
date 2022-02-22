a3075ca38f338eab59c46c89ef38e7ff8ec76401
Divino J. Silva <divino_j_silva@hotmail.com>
2022-02-22 12:46:30 -0300

    thaks

--- src/account-overview.jsx
+++ src/account-overview.jsx
@@ -11,7 +11,14 @@ export const AccountOverview = ({data}) => {
   return (
     <div className="AccountOverview">
       <FontAwesomeIcon icon={faHardHat} />
-      Build your components here
+      thanks for the opportunity, but my degree as a developer 
+      still requires a little more time for web development. 
+      even so I'm leaving a React Native project on my github if 
+      you can take a look at it, as it has great similarity with 
+      web development it can serve as a basis for me to be evaluated. 
+      I thank you in advance for the opportunity.
+      
+      https://github.com/cyberlinkrv/gameplay
 
     
     </div>
