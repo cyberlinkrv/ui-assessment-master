fe47eb5e85f443918fa46acd471512dce7ab692c
Divino J. Silva <divino_j_silva@hotmail.com>
2022-02-24 16:39:55 -0300

    imports bootstrap, for start

--- src/account-overview.jsx
+++ src/account-overview.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHardHat } from '@fortawesome/free-solid-svg-icons';
-import styled from 'styled-components';
-
 import './account-overview.css';
 
 export const AccountOverview = ({data}) => {
   console.log(data);
 
   return (
-    <div className="AccountOverview">
-      <FontAwesomeIcon icon={faHardHat} />
-      thanks for the opportunity, but my degree as a developer 
-      still requires a little more time for web development. 
-      even so I'm leaving a React Native project on my github if 
-      you can take a look at it, as it has great similarity with 
-      web development it can serve as a basis for me to be evaluated. 
-      I thank you in advance for the opportunity.
-      
-      https://github.com/cyberlinkrv/gameplay
+    <div className="DjsStyle">
 
+    
     
     </div>
   )
